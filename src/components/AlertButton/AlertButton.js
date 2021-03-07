0f40386a1725c65c06f5b1efb81dd88bdf4cf24a
fmuniente <frd.muniente@gmail.com>
2021-03-07 18:13:54 +0100

    net alerts v4

--- src/components/AlertButton/AlertButton.js
+++ src/components/AlertButton/AlertButton.js
@@ -1,10 +1,39 @@
-import React from 'react'
-import './AlertButton.css'
+import React, { Component } from "react";
+import { withAuth } from "./../../context/auth-context";
+import { Redirect } from 'react-router-dom'
+import "./AlertButton.css";
+import AlertService from "./../../services/alert-service";
 
-export default function AlertButton() {
-  return (
-    <div>
-      <button>Alert Button</button>
-    </div>
-  )
+class AlertButton extends Component {
+	
+  state = {
+    alertsent: false,
+    alertId: "",
+    alert: {}
+  }
+
+  handleClick = async () => {
+    const createdAlert = await AlertService.create()
+    
+    const alertId = createdAlert._id
+    this.setState({alertsent: true, alertId})
+	};
+
+  componentDidMount() {
+    this.setState({alertsent: false})
+  }
+
+	render() {
+    if (this.state.alertsent) return <Redirect to={`/alerts/${this.state.alertId}`} />
+
+		return (
+			<div className='alert-container' onClick={this.handleClick}>
+				<button className='alert-button' type='submit'>
+					Alert
+				</button>
+			</div>
+		);
+	}
 }
+
+export default withAuth(AlertButton);
--- src/pages/UserAlert/UserAlert.js
+++ src/pages/UserAlert/UserAlert.js
@@ -1,14 +1,41 @@
 import React, { Component } from "react";
-
+import { withAuth } from "./../../context/auth-context";
+import AlertService from "./../../services/alert-service"
 class UserAlert extends Component {
+
+  state = {
+    alert: {}
+  }
+
+  getActiveAlert = async () => {
+    const activeAlert = await AlertService.active(this.props.match.params.alertId)
+    console.log(activeAlert)
+    this.setState({ alert: activeAlert})
+  }
+
+  componentDidMount() {
+    this.getActiveAlert()
+  }
   render() {
-    const createdAt = this.props.user.userAlert.created_at.toString();
+    // const time = String(this.state.alert.created_at)
+    // const hour = time.slice(11, 19)
+    // const day = time.slice(0, 10);
+
     return (
-      <div>
-        <h4>Alert sent to:</h4>
-        <p>{createdAt}</p>
-      </div>
-    );
+			<div>
+				<h4>Alert sent at:</h4>
+				<h2>{this.state.alert.hour}</h2>
+				<h2>{this.state.alert.date}</h2>
+
+				<h4>To the following nets:</h4>
+				{this.props.user.nets.map((net) => {
+					return <h2 key={net._id}>{net.netname}</h2>;
+				})}
+
+				<button>Cancel, not an alert</button>
+				<button>I'm Ok, finist alert</button>
+			</div>
+		);
   }
 }
 
