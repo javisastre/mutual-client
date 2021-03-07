d6cb83466d50c63f7cc395d7f7625f781a5010fe
Javi Sastre <javisastrecal@gmail.com>
2021-03-07 16:51:36 +0100

    alert-service v1 and net-service fixes

---
+++ src/services/alert-service.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+class AlertService {
+  constructor() {
+    this.alert = axios.create({
+      baseURL: "http://localhost:5000/api/alerts",
+      withCredentials: true,
+    });
+  }
+
+  create() {
+    const pr = this.alert.post("/create").then(({ data }) => data);
+    return pr;
+  }
+}
+
+const alertService = new AlertService();
+export default alertService;
--- src/services/net-service.js
+++ src/services/net-service.js
@@ -3,28 +3,28 @@ import axios from "axios";
 class NetService {
   constructor() {
     this.net = axios.create({
-      baseURL: "http://localhost:5000",
+      baseURL: "http://localhost:5000/api/nets",
       withCredentials: true,
     });
   }
 
   create(netname, netcode) {
     const pr = this.net
-      .post("/api/nets/create", { netname, netcode })
+      .post("/create", { netname, netcode })
       .then(({ data }) => data);
     return pr;
   }
 
   join(netname, netcode) {
     const pr = this.net
-      .put("/api/nets/join", { netname, netcode })
+      .put("/join", { netname, netcode })
       .then(({ data }) => data);
     return pr;
   }
 
   leave(netId) {
     const pr = this.net
-      .post("/api/nets/leave", { value: netId })
+      .post("/leave", { value: netId })
       .then(({ data }) => data);
     return pr;
   }
